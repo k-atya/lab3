f1f2e55694e99692c83e374dd015b9171990972e
Katya <katherinenoble0@gmail.com>
2018-01-25 10:39:03 -0800

    Added interactions

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,44 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Experience Chan! It's not a lot of questions. \nToo many questions is the Chan disease.\nThe best way is just to observe the noise of the world.\nThe answer to your questions?\nAsk your own heart.");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+
 	});
 
+	$("#project1").click(projectClick);
+	$("#testjs").click(buttonClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	console.log("Clicked");
+	// var containingProject = $(this).closest(".project");
+  //   containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
+	var containingProject = $(this).closest(".project");
+	    var description = $(containingProject).find(".project-description");
+	    if (description.length == 0) {
+	       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	    } else {
+	      //  description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+				$(".project-description").fadeOut();
+	    }
+}
+
+function buttonClick(e) {
+	e.preventDefault();
+	console.log("Clicked");
+
+
 }
